b17f482042a918e1d9a9cc007625731c0494f957
alexaib <alexaib@outlook.es>
2022-06-01 11:43:36 +0200

    added update query test unit

---
+++ src/org/uem/dam/test/UpdateQueryTestUnit.java
@@ -0,0 +1,25 @@
+package org.uem.dam.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.uem.dam.GuiaMichelin.contract.RestauranteContract;
+import org.uem.dam.GuiaMichelin.contract.TableContract;
+import org.uem.dam.GuiaMichelin.utils.SQLQueryBuilder;
+
+class UpdateQueryTestUnit extends SQLQueryBuilder {
+
+	@Test
+	public void updateRestauranteQueryTest() {
+		String expectedValue = "UPDATE RESTAURANTES SET NOMBRE = ? WHERE ID = ?;";
+		assertEquals(expectedValue, buildUpdateQuery("RESTAURANTES", new String[] { "NOMBRE" }));
+	}
+
+	@Test
+	public void updateRestauranteEnumQueryTest() {
+		String expectedValue = "UPDATE RESTAURANTES SET NOMBRE = ?, DIRECCION = ? WHERE ID = ?;";
+		assertEquals(expectedValue, buildUpdateQuery(TableContract.RESTAURANTES.toString(),
+				new String[] { RestauranteContract.NOMBRE.toString(), RestauranteContract.DIRECCION.toString() }));
+	}
+
+}
