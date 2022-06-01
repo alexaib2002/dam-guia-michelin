7acb7b64c0f2f0f55551bf77207a00d9f7331bad
alexaib <alexaib@outlook.es>
2022-06-01 21:47:02 +0200

    connection definition on file implemented

--- src/org/uem/dam/GuiaMichelin/persist/DBConnection.java
+++ src/org/uem/dam/GuiaMichelin/persist/DBConnection.java
@@ -1,8 +1,12 @@
 package org.uem.dam.GuiaMichelin.persist;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
@@ -10,8 +14,20 @@ public class DBConnection {
 	private String url;
 
 	public DBConnection() {
-		driver = "org.sqlite.JDBC";
-		url = "jdbc:sqlite:db/UserDatabase.db";
+		FileInputStream fileInputStream;
+		try {
+			fileInputStream = new FileInputStream("dbconfig.properties");
+			Properties dbProperties = new Properties();
+			dbProperties.load(fileInputStream);
+			driver = dbProperties.getProperty("driver");
+			url = dbProperties.getProperty("url");
+		} catch (FileNotFoundException e) {
+			System.err.println("Database connection file not found");
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Error while reading the file");
+			System.exit(2);
+		}
 	}
 
 	public Connection getConnection() throws SQLException, ClassNotFoundException {
